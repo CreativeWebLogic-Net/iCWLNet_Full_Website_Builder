43b605196ed223f9e662a3582799ad7da0d2e7e1
danielruul78 <danielruul78@gmail.com>
2023-02-02 08:23:22 +1100

    Created SQL Install Script
    SQL Dump Cleaned Out

--- bcms/install/base64encode.php
+++
@@ -1,9 +0,0 @@
-﻿<?
-
-// get contents of a file into a string
-$filename = "./favicon.ico";
-$handle = fopen($filename, "r");
-$contents = fread($handle, filesize($filename));
-echo base64_encode($contents);
-fclose($handle);
-?>
