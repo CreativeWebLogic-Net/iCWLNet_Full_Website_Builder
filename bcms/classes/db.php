ab721d31353a25e676c757dbcaa4fcad4db4cf70
Daniel Ruul <danielruul78@gmail.com>
2023-03-01 18:01:45 +1100

    Update db.php for Full Website Install Version

--- bcms/classes/db.php
+++ bcms/classes/db.php
@@ -9,34 +9,15 @@
     //$DB['server_type'] = "Sqlite";
 		
     if($DB['server_type']=="MySQL"){
-        /*
-        $DB['server_tag']="db-default.php";
-        $DB['server_desc']="Private Server";
-        $DB['current_dir']="/var/www/html";
-        $DB['server_number']=4;
-        $DB['hostname']="localhost";
-        $DB['usernamedb']="Edit This";
-        $DB['passworddb']="Edit This";
-        $DB['dbName']="bubblelite2";
-        
-        $DB['server_tag']="db-w-d.php";
-        $DB['server_desc']="Cloud Server";
-        $DB['current_dir']="/home1/cwy0ek0e/public_html";
-        $DB['server_number']=1;
-        $DB['hostname']="localhost";
-        $DB['usernamedb']='cwy0ek0e_bubblel';
-        $DB['passworddb']='DickSux5841';
-        $DB['dbName']='cwy0ek0e_bubblelite2';
-        $DB['dbNames']=array('cwy0ek0e_bubblelite2','cwy0ek0e_takebookings','cwy0ek0e_partnerspro','cwy0ek0e_smsg');
-        */
+       
         $DB['server_tag']="db-localhost.php";
         $DB['server_desc']="Xampp Localhost";
         $DB['current_dir']="D:\Documents\Projects\Easy Bubble CMS\GitHub\iCWLNet_Central_Administration";
         $DB['server_number']=0;
-        $DB['hostname']="localhost";
-        $DB['usernamedb']='cwy0ek0e_bubblel';
-        $DB['passworddb']='DickSux5841';
-        $DB['dbName']='bubblelite2';
+        $DB['hostname']="sitemanage.info";
+        $DB['usernamedb']='sitemanage_danielruul78';
+        $DB['passworddb']='DickSux5841*';
+        $DB['dbName']='sitemanage_bubblelite2';
         //$DB['dbNames']=array('cwy0ek0e_bubblelite2','cwy0ek0e_takebookings','cwy0ek0e_partnerspro','cwy0ek0e_smsg');
         
     }
