6ca2cfd6a862545449e82dc79f25f7f323d42632
Daniel Ruul <danielruul78@gmail.com>
2023-03-03 17:56:47 +1100

    DB Example

--- bcms/classes/db.php
+++ bcms/classes/db.php
@@ -19,26 +19,6 @@
         $DB['passworddb']="Edit This";
         $DB['dbName']="bubblelite2";
         
-        $DB['server_tag']="db-w-d.php";
-        $DB['server_desc']="Cloud Server";
-        $DB['current_dir']="/home1/cwy0ek0e/public_html";
-        $DB['server_number']=1;
-        $DB['hostname']="localhost";
-        $DB['usernamedb']='cwy0ek0e_bubblel';
-        $DB['passworddb']='DickSux5841';
-        $DB['dbName']='cwy0ek0e_bubblelite2';
-        $DB['dbNames']=array('cwy0ek0e_bubblelite2','cwy0ek0e_takebookings','cwy0ek0e_partnerspro','cwy0ek0e_smsg');
-        */
-        $DB['server_tag']="db-localhost.php";
-        $DB['server_desc']="Xampp Localhost";
-        $DB['current_dir']="D:\Documents\Projects\Easy Bubble CMS\GitHub\iCWLNet_Central_Administration";
-        $DB['server_number']=0;
-        $DB['hostname']="localhost";
-        $DB['usernamedb']='cwy0ek0e_bubblel';
-        $DB['passworddb']='DickSux5841';
-        $DB['dbName']='bubblelite2';
-        //$DB['dbNames']=array('cwy0ek0e_bubblelite2','cwy0ek0e_takebookings','cwy0ek0e_partnerspro','cwy0ek0e_smsg');
-        
     }
     
 
@@ -48,8 +28,8 @@
         $DB['current_dir']="/var/www/html";
         $DB['server_number']=2;
         $DB['hostname']="localhost";
-        $DB['usernamedb']="postgres";
-        $DB['passworddb']="DickSux5841";
+        $DB['usernamedb']="Edit This";
+        $DB['passworddb']="Edit This";
         $DB['dbName']="cwy0ek0e_bubblelite2";
     }
 
